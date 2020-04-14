51a0c7624f57a66c5becacebbca3823577022e62
JasonP77 <llvslr77@gmail.com>
2020-04-14 12:41:29 -0400

    working on post destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,17 @@ class PostsController < ApplicationController
 	end
 	def create
 		# user = User.find(params[:id])
-		post = Post.create(user_id: 19, img:params[:img], content:params[:content])
+		post = Post.create(user_id: 22, img:params[:img], content:params[:content])
 		
 	end
 
 	def show
 		post = Post.find(params[:id])
 		render json: post
+	end
+
+	def destroy
+		post =Post.find(params[:id])
+		post.delete
 	end
 end
